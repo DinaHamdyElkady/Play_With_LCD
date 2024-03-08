477a562eb2c55f0fa344c6ef50dcfc778cbc323a
DinaHamdyElkady <145927508+DinaHamdyElkady@users.noreply.github.com>
2024-03-08 14:04:34 +0200

    Create HLCD_config.h

---
+++ HAL/HLCD_config.h
@@ -0,0 +1,23 @@
+#ifndef HLCD_CONFIG_H
+#define HLCD_CONFIG_H
+
+/*configuration for control pins*/
+
+//Rs pin
+#define HLCD_u8_RS_PIN              MDIO_u8_PIN_0
+#define HLCD_u8_RS_PORT             MDIO_u8_PORTD
+
+//Rw pin
+#define HLCD_u8_RW_PIN              MDIO_u8_PIN_1
+#define HLCD_u8_RW_PORT             MDIO_u8_PORTD
+
+//EN pin
+#define HLCD_u8_EN_PIN              MDIO_u8_PIN_2
+#define HLCD_u8_EN_PORT             MDIO_u8_PORTD
+
+/*data pins*/
+#define HLCD_u8_DATA_PORT           MDIO_u8_PORTC
+
+
+
+#endif
